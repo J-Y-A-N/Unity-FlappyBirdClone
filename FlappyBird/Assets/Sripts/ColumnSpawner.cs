f9e4c0dc1599645ac874268e0a80b6b4b2f3f2b1
Jiahe Yan <yjh020711@ucla.edu>
2020-08-19 10:17:02 +0800

    First Commit
    Added the flappy bird Unity project

---
+++ FlappyBird/Assets/Sripts/ColumnSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnSpawner : MonoBehaviour {
+
+	public GameObject columnPrefab;
+	public float minY, maxY;
+	
+	float timer;
+	public float maxTime;
+
+	// Use this for initialization
+	void Start () {
+
+		SpawnColumn();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		if (timer >= maxTime)
+        {
+			SpawnColumn();
+			timer = 0;
+        }
+
+	}
+
+	void SpawnColumn()
+    {
+		float randYPos = UnityEngine.Random.Range(minY, maxY);
+		GameObject newColumn = Instantiate(columnPrefab);
+		newColumn.transform.position = new Vector2(transform.position.x, randYPos);
+	}
+
+}
---
+++ FlappyBird/Assets/Sripts/MoveLeft.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+	public float speed;
+	public float LeftLimit;
+	float groundLength;
+
+	BoxCollider2D groundCollider;
+
+	// Use this for initialization
+	void Start () {
+
+		if (gameObject.CompareTag("Ground"))
+        {
+			groundCollider = GetComponent<BoxCollider2D>();
+			groundLength = groundCollider.size.x;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+
+		if (gameObject.CompareTag("Ground"))
+        {
+			if (transform.position.x <= -groundLength)
+			{
+				transform.position = new Vector2(transform.position.x + 2 * groundLength, transform.position.y);
+			}
+		}
+
+		if (gameObject.CompareTag("Column"))
+        {
+			if (transform.position.x <= LeftLimit)
+            {
+				Destroy(gameObject);
+            }
+        }
+		
+	}
+}
